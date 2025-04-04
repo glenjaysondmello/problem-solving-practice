fd29b4011ceddcaf8d9b70c0c643160fcab7b24c
Glen Jayson Dmello <glendmello04@gmail.com>
2025-04-04 23:56:25 +0530

    factors of number

---
+++ factors.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class factors {
+    public static void main(String[] args) {
+        int n = 40;
+        factors(n);
+    }
+
+    static void factors(int n) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (int i = 1; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                if (n / i == i) {
+                    System.out.println(i + " ");
+                } else {
+                    System.err.println(i + " ");
+                    arr.add(n / i);
+                }
+            }
+        }
+        for (int i = arr.size() - 1; i >= 0; i--) {
+            System.out.println(arr.get(i) + " ");
+        }
+    }
+}
