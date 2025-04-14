9c16cb0fce950547d1b932e720dd48f8f0482a5e
Glen Jayson Dmello <glendmello04@gmail.com>
2025-04-14 09:33:23 +0530

    search in string

---
+++ search/linear_search/searchString.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class searchString {
+    public static void main(String[] args) {
+        String str = "dmello";
+        char target = 'e';
+        System.out.println(search(str, target));
+    }
+
+    static boolean search(String str, char target) {
+        if(str.length() == 0) {
+            return false;
+        }
+
+        // for(int i = 0; i < str.length(); i++) {
+        //     if(str.charAt(i) == target) {
+        //         return true;
+        //     }
+        // }
+
+        for(char ch : str.toCharArray()) {
+            if(ch == target) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
