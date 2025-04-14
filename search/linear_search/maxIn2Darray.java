3564af19bfa48f76fabe45857cca798ba744be3a
Glen Jayson Dmello <glendmello04@gmail.com>
2025-04-14 10:47:34 +0530

    max in 2darray

--- search/linear_search/maxIn2Darray.java
+++ search/linear_search/maxIn2Darray.java
@@ -13,12 +13,20 @@ public class maxIn2Darray {
 
         int max = arr[0][0]; 
 
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                if (arr[i][j] > max) {
-                    max = arr[i][j];
+        // for (int i = 0; i < arr.length; i++) {
+        //     for (int j = 0; j < arr[i].length; j++) {
+        //         if (arr[i][j] > max) {
+        //             max = arr[i][j];
+        //         }
+        //     }
+        // }
+
+        for(int[] row : arr) {
+            for(int col : row) {
+                if(col > max) {
+                    max = col;
                 }
-            }
+            } 
         }
 
         return max;
