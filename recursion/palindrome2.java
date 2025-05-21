0140131273f8e9b82b0325ee188d0b48f98e2f3d
Glen Jayson Dmello <glendmello04@gmail.com>
2025-05-21 15:01:54 +0530

    palindrome 2

--- recursion/palindrome2.java
+++ recursion/palindrome2.java
@@ -1,8 +1,25 @@
 public class palindrome2 {
     public static void main(String[] args) {
-        int n = 1221;
-        
+        int n = 12121;
+        System.out.println(palin(n));
     }
 
-    
+    static boolean palin(int n) {
+        String str = Integer.toString(n);
+        int start = 0, end = str.length() - 1;
+
+        return helper(str, start, end);
+    }
+
+    static boolean helper(String str, int start, int end) {
+        if(start >= end) {
+            return true;
+        }
+
+        if(str.charAt(start) == str.charAt(end)) {
+            return helper(str, start + 1, end - 1);
+        } else {
+            return false;
+        }
+    }  
 }
