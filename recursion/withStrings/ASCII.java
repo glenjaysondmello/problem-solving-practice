334f93b1e506084fd8746495884b7619522cf136
Glen Jayson Dmello <glendmello04@gmail.com>
2025-05-27 19:36:53 +0530

    ascii subsets using recursion

--- recursion/withStrings/ASCII.java
+++ recursion/withStrings/ASCII.java
@@ -1,7 +1,14 @@
+import java.util.ArrayList;
+
 public class ASCII {
     public static void main(String[] args) {
         String str = "abc";
-        subset(str, "");
+        // subset(str, "");
+        ArrayList<String> arr = subsetA(str, "");
+        System.out.println(arr);
+        // int count = subsetC(str, "");
+        // System.out.println(count);
+
     }
 
     static void subset(String str, String pro) {
@@ -15,5 +22,42 @@ public class ASCII {
         subset(str.substring(1), pro + ch);
         subset(str.substring(1), pro);
         subset(str.substring(1), pro + (ch + 0));
+    }
+
+    static ArrayList<String> subsetA(String str, String pro) {
+        if(str.isEmpty()) {
+            ArrayList<String> arr = new ArrayList<>();
+            arr.add(pro);
+            return arr;
+        }
+
+        char ch = str.charAt(0);
+
+        ArrayList<String> left = subsetA(str.substring(1), pro + ch);
+        ArrayList<String> right = subsetA(str.substring(1), pro);
+
+        left.addAll(right);
+
+        ArrayList<String> ascii = subsetA(str.substring(1), pro + (ch + 0));
+
+        left.addAll(ascii);
+
+        return left;
+    }
+
+    static int subsetC(String str, String pro) {
+        if(str.isEmpty()) {
+            return 1;
+        }
+
+        char ch = str.charAt(0);
+
+        int count = 0;
+
+        count += subsetC(str.substring(1), pro + ch);
+        count += subsetC(str.substring(1), pro);
+        count += subsetC(str.substring(1), pro + (ch + 0));
+
+        return count;
     }
 }
