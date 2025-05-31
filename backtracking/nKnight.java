dec76a1b45253f80311aec3875ae440ee389a1a7
Glen Jayson Dmello <glendmello04@gmail.com>
2025-05-31 15:27:39 +0530

    N Queen and N Knight problem using backtracking

---
+++ backtracking/nKnight.java
@@ -0,0 +1,75 @@
+public class nKnight {
+    public static void main(String[] args) {
+        int n = 4;
+        boolean[][] board = new boolean[n][n];
+        System.out.println(knight(board, 0));
+    }
+
+    static int knight(boolean[][] board, int col) {
+        if(col == board.length) {
+            display(board);
+            System.out.println();
+            return 1;
+        }
+        
+        int count = 0;
+
+        for(int row = 0; row < board.length; row++) {
+            if(isSafe(board, row, col)) {
+                board[row][col] = true;
+                count += knight(board, col + 1);
+                board[row][col] = false;
+            }
+        }
+        return count;
+    }
+
+    static boolean isSafe(boolean[][] board, int row, int col) {
+        if(isValid(board, row - 2, col + 1)) {
+            if(board[row - 2][col + 1]) {
+                return false;
+            }
+        }
+
+        if(isValid(board, row - 2, col - 1)) {
+            if(board[row - 2][col - 1]) {
+                return false;
+            }
+        }
+
+        if(isValid(board, row - 1, col + 2)) {
+            if(board[row - 1][col + 2]) {
+                return false;
+            }
+        }
+
+        if(isValid(board, row - 1, col - 2)) {
+            if(board[row - 1][col - 2]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    static boolean isValid(boolean[][] board, int row , int col) {
+        if(row >= 0 && row < board.length && col >= 0 && col < board.length) {
+            return true;
+        } 
+
+        return false;
+    }
+
+    static void display(boolean[][] board) {
+        for(boolean[] row : board) {
+            for(boolean ele : row) {
+                if(ele) {
+                    System.out.print("K ");
+                } else {
+                    System.out.print("X ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
