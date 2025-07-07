e7a047bef3fbbfcd6b920eb13c0d6ebebb943da2
Glen Jayson Dmello <glendmello04@gmail.com>
2025-07-07 15:09:56 +0530

    cll

---
+++ linked_list/CLL.java
@@ -0,0 +1,74 @@
+public class CLL {
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public CLL() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    public void insert(int value) {
+        Node node = new Node(value);
+
+        if(head == null) {
+            head = node;
+            tail = node;
+            return;
+        }
+
+        tail.next = node;
+        node.next = head;
+        tail = node;
+    }
+
+    public void display() {
+        Node node = head;
+
+        if(head != null) {
+            do {
+                System.out.print(node.value + " -> ");
+                node = node.next;
+            } while(node != head);
+        }
+
+        System.out.println("HEAD");
+    }
+
+    public void delete(int val) {
+        Node node = head;
+
+        if(node == null) return;
+
+        if(node.value == val) {
+            head = node.next;
+            tail.next = head;
+            return;
+        }
+
+        do {
+            Node n = node.next;
+            if(n.value == val) {
+                node.next = n.next;
+                break;
+            }
+
+            node = node.next;
+        } while(node != head);
+    }
+
+    private class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+}
---
+++ linked_list/Main3.java
@@ -0,0 +1,12 @@
+public class Main3 {
+    public static void main(String[] args) {
+        CLL list = new CLL();
+
+        list.insert(3);
+        list.insert(7);
+        list.insert(48);
+        list.insert(30);
+
+        list.display();
+    }
+}
