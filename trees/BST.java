1eeeccded89ce1802ad782bb3b5ccf644d8fd2c7
Glen Jayson Dmello <glendmello04@gmail.com>
2025-07-15 12:17:44 +0530

    travesals

--- trees/BST.java
+++ trees/BST.java
@@ -100,4 +100,47 @@ public class BST {
         display(node.left, "Left child of" + node.getValue() + " : ");
         display(node.right, "Right child of" + node.getValue() + " : ");
     }
+
+    public void preOrder() {
+        preOrder(root);
+    }
+
+    private void preOrder(Node node) {
+        if (node == null)
+            return;
+
+        System.out.print(node.value + " ");
+
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    public void postOrder() {
+        postOrder(root);
+    }
+
+    private void postOrder(Node node) {
+        if (node == null)
+            return;
+
+        postOrder(node.left);
+        postOrder(node.right);
+
+        System.out.print(node.value + " ");
+    }
+
+    public void inOrder() {
+        inOrder(root);
+    }
+
+    private void inOrder(Node node) {
+        if (node == null)
+            return;
+
+        inOrder(node.left);
+
+        System.out.print(node.value + " ");
+
+        inOrder(node.right);
+    }
 }
